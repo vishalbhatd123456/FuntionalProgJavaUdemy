8375078cf71eb2a027e2c59914b22b10ad01da7a
Vishal D Bhat <60818841+vishalbhatd123456@users.noreply.github.com>
2022-02-22 12:38:47 +0530

    Create P2FuncProgEven.java

---
+++ P2FuncProgEven.java
@@ -0,0 +1,15 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        printAllNumbersInListFunctionalEven(List.of(12,9,13,4,6,2,4,12,15)); ber%2 == 0;
+    }
+    private static boolean isEven(int number){
+        return num
+    }
+    private static void printAllNumbersInListFunctionalEven(List<Integer> numbers){
+        numbers.stream()
+        .filter(HelloWorld :: isEven).forEach(System.out::println);
+    }
+}
